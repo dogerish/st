ce38fe2e17c74c2cfb65b45eca3f494c6672b4db
dogerish <luca.filmstrip@gmail.com>
2022-06-29 16:26:53 -0400

    Fix broken link in documentation for STClanMember

--- src/interface/clanmember.ts
+++ src/interface/clanmember.ts
@@ -1,7 +1,10 @@
 /** Structure representing a member of a clan. */
 export interface STClanMember
 {
-	/** The name of the member. Can be resolved into an [STPlayer](STPlayer) */
+	/**
+	 * The name of the member. Can be resolved into an {@link 
+	 * class/player!STPlayer}
+	 */
 	name: string;
 	/** The date at which the member was last seen */
 	lastseen: Date;
